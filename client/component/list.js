f6ffd9280912dc2d9407b5edc5009c214336b706
Dwivedi <577730@cognizant.com>
2018-03-12 18:58:08 +0530

    with some styling

--- client/component/list.js
+++ client/component/list.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
 class List extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
+        this.state = {
             cName: "",
         }
     }
-show(item){
-    console.log(item.name);
-    if(item.msg.from!= item.name){
-        //alert("message from user 1");
-       // this.setState({cName : 'msg-card-li' })
-       console.log("check Array obj", item.msg.msg);
-      return <div className="msg-card-li-incoming">{item.msg.msg}</div>
+    show(item) {
+        console.log(item.name);
+        if (item.msg.from != item.name) {
+            //alert("message from user 1");
+            // this.setState({cName : 'msg-card-li' })
+            console.log("check Array obj", item.msg.msg);
+            return(
+            <div className="msg-card-li-incoming">
+                <div className="user-detail">{item.msg.from}</div>
+                <div className="user-message">{item.msg.msg}</div>
+            </div>);
+        }
+        else {
+            return <div className="msg-card-li-outgoing">
+                <div className="user-message">{item.msg.msg}</div>
+            </div>
+        }
     }
-    else{
-        return <div className="msg-card-li-outgoing">{item.msg.msg}</div>
-    }
-}
     render(props) {
-          return (
+        return (
             <ul className="msg-card">
                 {
                     this.props.items.map((item) => <li >{this.show(item)}</li>)
